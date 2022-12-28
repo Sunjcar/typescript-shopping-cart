05d704c671913f0bfa286b17abd92be9613d852e
Raymond Chen <raychen358@gmail.com>
2022-12-27 23:01:20 -0800

    Updated Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
 import Nav from "./Components/Nav"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Home  from "./Components/Home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./Components/Home"
 import Footer from "./Components/Footer"
-import { useEffect} from "react"
+import {Catalog} from "./Components/Catalog"
 import { Product } from "./Products"
 
 
 const App = (): JSX.Element => {
 
-
-  useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-    .then(res => res.json())
-    .then(data => console.log(data))
-  }, [])
-
   return (
-    <div className="flex flex-col h-[100vh]">
-     <Router>
-      <Nav/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="catalog" element={<Product/>}/>
-      </Routes>
-     </Router>
-     <Footer/>
-    </div>
+      <div className="flex flex-col h-[100vh]">
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="catalog" element={<Product/>} />
+          </Routes>
+        <Footer />
+      </div>
   )
 }
 
---
+++ src/BrowserRouter.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter as Router } from "react-router-dom"
+import App from "./App"
+
+export const BrowserRouter = () => {
+  return (
+    <Router>
+        <App/>
+    </Router>
+  )
+}
