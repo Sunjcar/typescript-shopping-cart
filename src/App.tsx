08539b39259d69fa61f6c295b09a8316322fa247
Raymond Chen <raychen358@gmail.com>
2022-12-27 17:02:07 -0800

    API FETCH

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,26 @@ import Nav from "./Components/Nav"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home  from "./Components/Home"
 import Footer from "./Components/Footer"
+import { useEffect} from "react"
+import { Product } from "./Products"
 
-const App = () => {
+
+const App = (): JSX.Element => {
+
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }, [])
+
   return (
     <div className="flex flex-col h-[100vh]">
      <Router>
       <Nav/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="catalog" element={<Product/>}/>
       </Routes>
      </Router>
      <Footer/>
---
+++ src/Products/index.tsx
@@ -0,0 +1,9 @@
+export const Product : React.FC = (): JSX.Element => {
+    return(
+        <div className="flex flex-wrap items-center justify-center flex-1">
+            <div className=" w-full h-[10rem]">
+                <img className="max-h-[15rem] " src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"/>
+            </div>
+        </div>
+    )
+}
