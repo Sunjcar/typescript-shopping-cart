d9bf56dac5da23f43ea0ec9676f8a70b23f35139
Raymond Chen <raychen358@gmail.com>
2023-01-05 22:03:48 -0800

    Button changes to purple if added to cart

--- src/Utilities/Button.tsx
+++ src/Utilities/Button.tsx
@@ -15,10 +15,22 @@ export const Button: React.FC<ButtonProps> = ({
 
         const action: string = added ? "REMOVE" : "ADD_TO_CART";
         dispatch && dispatch({ type: action, payload: ProductId })
-    } 
+    }
+
+
+    let style = {
+        backgroundColor: "#1da1f2"
+    }
+    if (added === true) {
+        style = {
+            backgroundColor: "#9333ea"
+        }
+    }
+
+
 
     return (
-        <button onClick={handleClick} className=" relative flex items-center justify-start p-4 overflow-hidden transition-all bg-[#1da1f2] hover:bg-white group">
+        <button style={style} id="button" onClick={handleClick} className=" relative flex items-center justify-start p-4 overflow-hidden transition-all bg-[#1da1f2] hover:bg-white group">
             <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-500 ease-out bg-purple-600 rounded group-hover:w-full group-hover:h-full -z-1"></span>
             <span className="z-10 w-full text-black transition-colors duration-300 ease-in-out group-hover:text-white">
                 {
